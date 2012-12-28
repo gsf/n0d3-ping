c1bf46daba28128b27baed5de889ae64335c4f54
Gabriel Farrell <g@grrawr.com>
2012-12-27 21:54:49 -0500

    Adjusted for changes to n0d3

--- ping.js
+++ ping.js
@@ -1,8 +1,9 @@
-module.exports = function ping (room, log) {
-  room.on('command', function (message) {
-    if (message.indexOf('ping') != -1) {
-      room.say('pong');
-      log('Ponged a ping');
-    }
+module.exports = function ping (client, n0d3) {
+  client.on('.ping', function (command) {
+    command.reply('pong');
+    n0d3.log('Ponged a ping');
+  });
+  client.on('?ping', function () {
+    client.say('ping - ping the bot and get a pong - https://github.com/gsf/n0d3-ping');
   });
 };
